d28ff2c128ffd1483012ae8cda66bbdab4a30a3e
Ezequiel Deluca <ezequiel.deluca@shopify.com>
2022-08-31 13:58:06 -0400

    Apply flocking rules to a few instances

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,10 +1,6 @@
 class House
   def line(number)
     case number
-    when 2
-      "This is the malt that lay in the house that Jack built.\n"
-    when 3
-      "This is the rat that ate the malt that lay in the house that Jack built.\n"
     when 4
       "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 5
@@ -24,8 +20,12 @@ class House
     when 12
       "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     else
-      "This is the house that Jack built.\n"
+      "This is #{phrase(number)}the house that Jack built.\n"
     end
+  end
+
+  def phrase(number)
+      ["the rat that ate ", "the malt that lay in ", ""].last(number).join("")
   end
 
   def recite
